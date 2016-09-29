10baef9137f1571ed3a02650fccf9d4310c5de68
Roy Meissner <roy-meissner@gmx.net>
2016-09-29 14:52:32 +0200

    Fixed the missing module

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const co = require('../common');
+
 module.exports = {
   'user': {
     uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'userservice.experimental.slidewiki.org',
