3e392dc909f6d09961b29d6a16b899aa71e5bc16
dpaun <dpaunovic@gmail.com>
2017-02-13 15:33:13 +0100

    Added the missing notification uri

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -6,6 +6,9 @@ module.exports = {
   'deck': {
     uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice'
   },
+  'notification': {
+    uri: (!co.isEmpty(process.env.SERVICE_URL_NOTIFICATION)) ? process.env.SERVICE_URL_NOTIFICATION : 'http://notificationservice'
+  },
   'user': {
     uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'http://userservice'
   }
