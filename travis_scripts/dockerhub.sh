ec2353db7a922047e1f15a4c08a00444ba97b543
Ben Wulff <benjamin.wulff@iais.fraunhofer.de>
2017-06-13 04:25:20 +0200

    Fixing image building

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t slidewiki/activitiesservice ./
+docker build -t slidewiki/activitiesservice:latest-dev ./
 docker push slidewiki/activitiesservice:latest-dev
