11f5550d071b5770c3fa300a0a045c959ff2eb47
Stavros Maroulis <stavrosmrl@gmail.com>
2018-06-20 17:05:49 +0300

    Add username to lrs activities

--- application/lib/xapiUtil.js
+++ application/lib/xapiUtil.js
@@ -7,18 +7,13 @@ module.exports = {
   actor: function(user) {
     return {
       objectType: 'Agent',
-      // name: user.username,
-
       // TODO Investigate how LRS can manage anonymous data (unregistered users)
-
       mbox_sha1sum: user.mbox_sha1,
-      // account: {
-      //   homePage : Microservices.platform.uri,
-      //   name: user.username,
-      // },
-
+      account: {
+        homePage : Microservices.platform.uri,
+        name: user.username,
+      }
     };
-
-  },
+  }
 
 };
