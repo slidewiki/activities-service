c4ef9c3f5f8bc84f1aaf11d6baeca738ee1c8c60
Kostis Pristouris <kprist@gmail.com>
2018-06-21 10:33:27 +0300

    Really add username to lrs activities; remove mbox_sha1sum

--- application/lib/xapiUtil.js
+++ application/lib/xapiUtil.js
@@ -7,13 +7,18 @@ module.exports = {
   actor: function(user) {
     return {
       objectType: 'Agent',
+
       // TODO Investigate how LRS can manage anonymous data (unregistered users)
-      mbox_sha1sum: user.mbox_sha1,
+
+      // TODO should users be able to select whether their account is shown here or not ?
+      // mbox_sha1sum: user.mbox_sha1,
+
       account: {
         homePage : Microservices.platform.uri,
         name: user.username,
-      }
+      },
+
     };
-  }
+  },
 
 };
